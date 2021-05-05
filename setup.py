4d3c214ee52338bee3e472c8a54560e8276e63fb
Steve De Jongh <sdejongh@he2b.be>
2021-05-05 10:20:19 +0200

    Corrected Package for V1

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ setup(
   author = 'Steve De Jongh',                   # Type in your name
   author_email = 'dejongh.st@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/sdejongh/universal_answer/raw/master/dist/Universal%20Answer-1.0.tar.gz',    # I explain this later on
   keywords = ['h2g2', 'useless', 'dontuse'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
       ],
   classifiers=[
-    'Development Status :: 5 - Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Nobody',      # Define that your audience are developers
+    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Other Audience',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
